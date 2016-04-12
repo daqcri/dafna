c4c88cf6a8329b54766776172ef5039f0f254938
Hossam Hammady <github@hammady.net>
2016-04-12 09:20:00 +0300

    hardcoding GA tracking id

--- static/google_analytics.js
+++ static/google_analytics.js
@@ -5,6 +5,6 @@ function GoogleAnalytics() {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '<%= ENV["GOOGLE_ANALYTICS_ID"] %>', 'auto');
+  ga('create', 'UA-55160701-3', 'auto');
   ga('send', 'pageview');
 }
